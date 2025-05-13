430f5f282892a21e04b123d84110aab4d7ac8bfb
shoptaki <vikas.sanhotra70@gmail.com>
2025-05-12 17:10:09 -0700

    Add .gitignore and initial project setup

---
+++ expenses/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title =  models.CharField(max_length=255)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    
+    def __str__(self):
+        return f"{self.title} - $(self.amount)"
